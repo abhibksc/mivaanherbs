c7ace736ffaab0f59f0e1f4fab297adbbbf67747
Abhishek Kumar <155058004+abhibksc@users.noreply.github.com>
2025-07-30 13:48:05 +0530

    updated

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,6 @@ const userRouter = express.Router();
 
 const bcrypt = require("bcrypt");
 const User = require("../models/auth.js");
-const Transaction = require("../models/Transaction.js");
 
 // Generate username
 function generateUsername(fullName) {
